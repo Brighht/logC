51865bf314afdeaf10aff4af8dfc946dafafa1e0
Brighht <bright.andohh@gmail.com>
2024-11-26 13:40:55 -0600

    "02"

--- library.c
+++ library.c
@@ -2,8 +2,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void process_line(char *line);
+void analyze_line(char *line);
 
 int main(int const argc, char ** argv) {
     if (argc != 2) {
@@ -26,4 +28,35 @@ int main(int const argc, char ** argv) {
     //close logfile
     fclose(logfile);
     return EXIT_SUCCESS;
+}
+
+/**
+ * Function: analyze_line
+ * ----------------------
+ * Analyzes a single line from the log file.
+ */
+
+void analyze_line(const char *line) {
+    // Example: Check for a specific keyword in the line
+    if (strstr(line, "ERROR")) {
+        printf("[ALERT] Found an error: %s\n", line);
+        //todo: ERROR logic here
+    }
+    else if (strstr(line, "INFO")) {
+        printf("[INFO] General information: %s\n", line);
+        //todo: INFO logic here
+    }
+    else {
+        printf("[DEBUG] Other line: %s\n", line);
+        //todo: DEBUG logic here
+    }
+}
+
+void process_line(char * line) {
+  int len = strlen(line);
+  char *token = strtok(line, " ");
+  while (token != NULL) {
+	analyze_line(token);
+   	token = strtok(NULL, ", ");
+  }
 }
